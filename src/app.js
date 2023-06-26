5cd61dfa317b75bba8dd220ffd2f715cf26c9e96
Dilasa Thapa <thapadilasa71@gmail.com>
2023-06-26 18:23:56 +0530

    done

--- src/app.js
+++ src/app.js
@@ -24,16 +24,32 @@ const PLAYERS = [
 // initialize players with image and strength
 const initPlayers = (players) => {
     let detailedPlayers = [];
-    // Create players using for loop
-    // Type your code here
+
+    for (let i = 0; i < players.length; i++) {
+        var obj = {}
+        obj.name = players[i]
+        obj.strength = getRandomStrength()
+        obj.image = `../images/super-${i+1}.png`
+        
+        // let num = Math.floor((Math.random()*10)+1);
+        // console.log(num)
+        
+        if (i%2 == 0) {
+            obj.type = "hero"
+        } else {
+            obj.type = "villain"
+        }
+        detailedPlayers.push(obj)
+
+    }
 
     return detailedPlayers;
 }
 
 // getting random strength
 const getRandomStrength = () => {
-    // Return a random integer (0,100]
-    // Note: You can use Math.random() and Math.ceil()
+    let strengthnum = Math.floor((Math.random() * 100) + 1);
+    return strengthnum;
 }
 
 const buildPlayers = (players, type) => {
@@ -43,12 +59,28 @@ const buildPlayers = (players, type) => {
     // depending of type of player(hero|villain)
     // Type your code here
 
+    for (let i = 0; i < players.length; i++) {
+
+        if (players[i].type == type) {
+            
+            let text = `<div class="player">
+            <img src="${players[i].image}" alt="">
+            <div class="name">${players[i].name}</div>
+            <div class="strength">${players[i].strength}</div>
+            </div>`
+
+            fragment = fragment + text;
+
+        }
+        // console.log();
+    }
     return fragment;
 }
 // Display players in HTML
 const viewPlayers = (players) => {
 
-    document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
+    document.getElementById('heroes').innerHTML =  buildPlayers(players, 'hero')
+
     document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
 
 }
